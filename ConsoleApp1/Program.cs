c3fd91ed3e7bea39494ac1b03025dde432e6e419
stevewhocodes <ironmonkus@gmail.com>
2025-09-20 20:25:53 -0400

    latest c# test apps

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Print title
+Console.WriteLine("Welcome to Temperature Conversion!");
+Console.WriteLine("==================================");
+Console.WriteLine("Available units are (C)elsius, (F)ahrenheit, and (K)elvin\n");
 
-Console.WriteLine("Hello, World!");
+// prompt for temperature
+Console.Write("Enter a temperature: ");
+double temperature = double.Parse(Console.ReadLine());
+
+// prompt for unit
+Console.Write("Enter Unit: ");
+string unit = Console.ReadLine().ToUpper();
+
+// prompt for conversion unit
+Console.Write("Enter Conversion Unit: ");
+string conversionUnit = Console.ReadLine().ToUpper();
+
+// blank space
+Console.WriteLine();
+
+
+
+// convert if needed
+double converted = 0;
+
+if (unit != "C" && unit != "F" && unit != "K")
+{
+    Console.WriteLine($"Please enter a valid unit! {unit} is not a valid unit!");
+}
+else if (conversionUnit != "C" && conversionUnit != "F" && conversionUnit != "K")
+{
+    Console.WriteLine($"Please enter a valid unit! {conversionUnit} is not a valid conversion unit!");
+}
+else if (unit == conversionUnit)
+{
+    Console.WriteLine($"No conversion needed to convert {unit} to {conversionUnit}.");
+}
+else
+{
+
+    if (unit == "C")
+    {
+        if (conversionUnit == "F")
+        {
+            converted = temperature * (9.0 / 5.0) + 32.0;
+            Console.WriteLine($"A temperature of {temperature}{unit} is {Math.Round(converted, 2)}{conversionUnit}");
+        }
+        else if (conversionUnit == "K")
+        {
+            converted = temperature + 273.15;
+            Console.WriteLine($"A temperature of {temperature}{unit} is {Math.Round(converted, 2)}{conversionUnit}");
+        }
+    }
+
+    else if (unit == "F")
+    {
+        if (conversionUnit == "C")
+        {
+            converted = (temperature - 32.0) * (9.0 / 5.0);
+            Console.WriteLine($"A temperature of {temperature}{unit} is {Math.Round(converted, 2)}{conversionUnit}");
+        }
+        else if (conversionUnit == "K")
+        {
+            converted = (temperature - 32.0) * (5.0 / 9.0) + 273.15;
+            Console.WriteLine($"A temperature of {temperature}{unit} is {Math.Round(converted, 2)}{conversionUnit}");
+        }
+    }
+
+    else if (unit == "K")
+    {
+        if (conversionUnit == "C")
+        {
+            converted = temperature - 273.15;
+            Console.WriteLine($"A temperature of {temperature}{unit} is {Math.Round(converted, 2)}{conversionUnit}");
+        }
+        else if (conversionUnit == "F")
+        {
+            converted = (temperature - 273.15) * (9.0 / 5.0) + 32.0;
+        }
+
+    }
+
+    Console.WriteLine($"A temperature of {temperature}{unit} is {Math.Round(converted, 2)}{conversionUnit}");
+
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿// prompt user for HTTP status code
+Console.Write("Enter a Response Code: ");
+int input = int.Parse(Console.ReadLine());
+
+// Check response and print our response type
+// switch (input)
+// {
+//     case >= 100 and <= 199:
+//         Console.WriteLine($"{input} is a Informational response.");
+//         break;
+//     case > 199 and <= 299:
+//         Console.WriteLine($"{input} is a Successful response.");
+//         break;
+//     case > 299 and <= 399:
+//         Console.WriteLine($"{input} is a Redirection response.");
+//         break;
+//     case > 399 and <= 499:
+//         Console.WriteLine($"{input} is a Client error response.");
+//         break;
+//     case > 499 and <= 599:
+//         Console.WriteLine($"{input} is a Server error response.");
+//         break;
+//     default:
+//         Console.WriteLine($"{input} is not a valid response.");
+//         break;
+// }
+
+string result = input switch
+{
+    >= 100 and <= 199 => $"{input} is a Informational response.",
+    > 199 and <= 299 => $"{input} is a Successful response.",
+    > 299 and <= 399 => $"{input} is a Redirection response.",
+    > 399 and <= 499 => $"{input} is a Client error response.",
+    > 499 and <= 599 => $"{input} is a Server error response.",
+    _ => $"{input} is not a valid response."
+    
+};
+Console.WriteLine(result);

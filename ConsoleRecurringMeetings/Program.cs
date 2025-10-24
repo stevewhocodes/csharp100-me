4f46dd02fb0d9a854fe76e53b82b6b412fb63633
gitmonkus <ironmonkus@gmail.com>
2025-10-24 12:48:45 -0400

    distrohop

---
+++ ConsoleRecurringMeetings/Program.cs
@@ -0,0 +1,118 @@
+﻿DateTime startDate, endDate;
+String frequency;
+String weekends = "N";
+// Prompt for start date
+do
+{
+    Console.Write("Enter start date: ");
+
+    if (DateTime.TryParse(Console.ReadLine(), out startDate))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid Date.");
+    }
+} while (true);
+
+// Prompt for end date
+
+do
+{
+    Console.Write("Enter end date: ");
+    if (DateTime.TryParse(Console.ReadLine(), out endDate))
+    {
+        if (endDate > startDate)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("End date needs to be greater than start date.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Invalid Date.");
+    }
+} while (true);
+
+// Prompt for Frequency (D,W,M,Y)
+
+
+do
+{
+    Console.Write("Enter Frequency [(D)aily, (W)eekly, (M)onthly, (Y)early]: ");
+    frequency = Console.ReadLine().ToUpper();
+    if (frequency == "D" || frequency == "W" || frequency == "M" || frequency == "Y")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid Frequency.");
+    }
+} while(true);
+
+
+// prompt to skip weekends
+if (frequency == "D")
+{
+    do
+    {
+        Console.Write("Skip Weekends (Y/N): ");
+        weekends = Console.ReadLine().ToUpper();
+        if (weekends == "Y" || weekends == "N")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Weekends.");
+        }
+    }while (true);
+}
+
+
+
+// Keep track of current date while looping
+DateTime current = startDate;
+
+// print out header and first date
+Console.WriteLine("\nMeeting Dates");
+Console.WriteLine("=============");
+
+
+while (current <= endDate)
+{
+    // print out date
+    Console.WriteLine($"{current:dddd MMM d, yyyy}");
+   
+    switch (frequency)
+    {
+        case "D":
+            current = current.AddDays(1);
+            if (weekends == "Y" && current.DayOfWeek == DayOfWeek.Saturday)
+            {
+                current = current.AddDays(2);
+            }
+            else if (weekends == "Y" && current.DayOfWeek == DayOfWeek.Sunday)
+            {
+                current = current.AddDays(1);
+            }
+            break;
+        case "W":
+            current = current.AddDays(7);
+            break;
+        case "M": 
+            current = current.AddMonths(1);
+            break;
+        case "Y":
+            current = current.AddYears(1);
+            break;
+    }
+    
+
+}
---
+++ refactorRecurringMeeting/Program.cs
@@ -0,0 +1,119 @@
+﻿// variables for start, end, frequency, and skipping weekends
+DateTime start, end;
+string frequency;
+string skipweekends = "N";
+
+// prompt for start date
+
+do
+{
+    Console.Write("Enter start date: ");
+    if (DateTime.TryParse(Console.ReadLine(), out start))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("That is not a valid date!");
+    }
+}while(true);
+
+// prompt for end date, check if > start date
+
+do
+{
+    Console.Write("Enter end date: ");
+    if (DateTime.TryParse(Console.ReadLine(), out end))
+    {
+        if (end > start)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("End date must be after start date!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("That is not a valid date!");
+    }
+}while(true);
+
+// prompt for frequency
+
+do
+{
+    Console.Write("Enter frequency [(D)aily, (W)eekly, (M)onthly, (Y)early]: ");
+    frequency = Console.ReadLine().ToUpper();
+
+    if (frequency == "D" || frequency == "W" || frequency == "M" || frequency == "Y")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("That is not a valid frequency!");
+    }
+}while(true);
+
+// prompt for weekends if frequency is daily
+if (frequency == "D")
+{
+    do
+    {
+        Console.Write("Skip weekends (Y/N): ");
+        skipweekends = Console.ReadLine().ToUpper();
+        
+        if (skipweekends == "Y" || skipweekends == "N")
+        {
+            break;
+        }else
+        {
+            Console.WriteLine("That is not a valid entry!");
+        }
+    }while(true);
+}
+
+// print header
+Console.WriteLine("\nMeetingDates");
+Console.WriteLine("=============");
+
+// set first date
+DateTime current = start;
+
+// loop until end date is reached
+while (current <= end)
+{
+    // print first date, format from string formatting lesson
+    Console.WriteLine($"{current:dddd MMM d, yyyy}");
+
+    switch (frequency)
+    {
+        // handle weekends if necessary by adding additional days (Daily)
+        case "D":
+            current = current.AddDays(1);
+            if (skipweekends == "Y" && current.DayOfWeek == DayOfWeek.Saturday)
+            {
+                current = current.AddDays(2);
+            }
+            else if (skipweekends == "Y" && current.DayOfWeek == DayOfWeek.Sunday)
+            {
+                current = current.AddDays(1);
+            }
+
+            break;
+        // add 7 days
+        case "W":
+            current = current.AddDays(7);
+            break;
+        // add 1 month
+        case "M":
+            current = current.AddMonths(1);
+            break;
+        // add 1 year
+        case "Y":
+            current = current.AddMonths(1);
+            break;
+    }
+}

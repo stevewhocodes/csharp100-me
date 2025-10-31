bf4f0a74c0b02c2c73d8d99c5ecd4af813e6aa81
ironmonkus <ironmonkus@debi.lan>
2025-10-31 16:52:34 -0400

    lockerRental

---
+++ test/Program.cs
@@ -0,0 +1,18 @@
+﻿string str = "xaxxaxaxx";
+
+int count = 0;
+string lastTwo = str[^2..];
+
+
+Console.WriteLine("This is the last two.");
+Console.WriteLine(lastTwo);
+
+for (int i = 0; i < str.Length - 2; i++)
+{
+    if (str[i..(i + 2)] == lastTwo)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
